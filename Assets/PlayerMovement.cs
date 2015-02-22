628f3df0c3b3a8de034d33ba18ed5111e9c70940
jjcross <jjcross@umich.edu>
2015-02-22 18:52:20 -0500

    jacks stuff

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float speed = 50.0f;
+	public float jump = 500.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			rigidbody.AddForce (new Vector3 (0, jump, 0));
+		}
+	}
+
+	void FixedUpdate() {
+		float move = Input.GetAxis ("Horizontal");
+		if (move != 0) {
+			rigidbody.velocity = new Vector2 (move * speed, rigidbody.velocity.y);
+		}
+	}
+}
