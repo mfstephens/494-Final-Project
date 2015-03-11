f17412a815a559e0d3d11ad916f726423457f3be
skarl3 <skarl@umich.edu>
2015-03-11 07:20:48 -0400

    newest

---
+++ Assets/ScottStuff/Elevator.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Elevator : MonoBehaviour {
+
+	public float timeInterval = 5.5f;
+	public float moveSpeed = 1f;
+	float downPosition, upPosition, startTimer;
+	bool down = false;
+	// Use this for initialization
+	void Start () {
+		downPosition = -99f;
+		upPosition = 0f;
+		startTimer = Time.time;
+	}
+	
+
+	void FixedUpdate () {
+		if (startTimer + timeInterval < Time.time){
+			if(down){
+				Vector3 moveTemp = transform.position;
+				moveTemp.y += moveSpeed;
+				transform.position = moveTemp;
+				if(moveTemp.y >= upPosition){
+					down = false;
+					startTimer = Time.time;
+				}
+			}
+			else if(!down){
+				Vector3 moveTemp = transform.position;
+				moveTemp.y -= moveSpeed;
+				transform.position = moveTemp;
+				if(moveTemp.y <= downPosition){
+					down = true;
+					startTimer = Time.time;
+				}
+			}
+
+		}
+	}
+}
