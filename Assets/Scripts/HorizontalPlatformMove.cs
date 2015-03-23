52031b4c56edef17e169de28dd00bda9441c5112
jjcross <jjcross@umich.edu>
2015-03-23 15:25:50 -0400

    first push

---
+++ Assets/Scripts/HorizontalPlatformMove.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HorizontalPlatformMove : MonoBehaviour {
+
+	public float platformMin, platformMax, moveSpeed;
+	
+	// Update is called once per frame
+	void Update () {
+		if ((this.transform.position.x <= platformMin) || (this.transform.position.x >= platformMax)) {
+			moveSpeed = moveSpeed * -1;
+		}
+		this.transform.position += new Vector3 (moveSpeed, 0, 0);
+	}
+
+}
